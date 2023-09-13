91e56448be938e2421be60744c560604abfa732a
Rene Leeman <leeman.rene@gmail.com>
2023-09-13 21:56:15 +0300

    final update

--- src/chat/Server.java
+++ src/chat/Server.java
@@ -1,13 +1,19 @@
 package chat;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.*;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Server {
 
     private ServerSocket ss;
+    private final Set<PrintWriter> clientWriters = Collections.synchronizedSet(new HashSet<>());
 
     public Server() {
         try {
@@ -18,7 +24,11 @@ public class Server {
                 Socket soc = ss.accept();
                 System.out.println("Connection established with: " + soc.getInetAddress().getHostAddress());
 
-                new Thread(new ClientHandler(soc)).start();
+                PrintWriter out = new PrintWriter(soc.getOutputStream(), true);
+                synchronized(clientWriters) {
+                    clientWriters.add(out);
+                }
+                new Thread(new ClientHandler(soc, out)).start();
             }
 
         } catch (Exception e) {
@@ -26,26 +36,39 @@ public class Server {
         }
     }
 
+    private void broadcastMessage(String message) {
+        synchronized(clientWriters) {
+            for (PrintWriter writer : clientWriters) {
+                writer.println(message);
+            }
+        }
+    }
 
     private class ClientHandler implements Runnable {
         private Socket clientSocket;
+        private PrintWriter clientOut;
 
-        public ClientHandler(Socket socket) {
+        public ClientHandler(Socket socket, PrintWriter out) {
             this.clientSocket = socket;
+            this.clientOut = out;
         }
 
         @Override
         public void run() {
             try {
                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
 
                 String str;
                 while ((str = in.readLine()) != null) {
-                    out.println(str);
+                    System.out.println("Received: " + str);
+                    broadcastMessage(str);
                 }
 
+                synchronized(clientWriters) {
+                    clientWriters.remove(clientOut);
+                }
                 clientSocket.close();
+
             } catch (IOException e) {
                 e.printStackTrace();
             }
